a3fd9b6fad6faaca5cafd56b3fac2eaee536a145
Rasalahmad <rasalahmadsit@gmail.com>
2022-08-04 22:28:52 +0600

    connect with mongoose

--- app.js
+++ app.js
@@ -18,13 +18,13 @@ const app = express();
 dotenv.config();
 
 // database connection
-// mongoose
-//   .connect(process.env.MONGO_CONNECTION_STRING, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log("Database connected successful"))
-//   .catch((err) => console.log(err));
+mongoose
+  .connect(process.env.MONGO_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Database connected successful"))
+  .catch((err) => console.log(err));
 
 // request parser
 app.use(express.json());
